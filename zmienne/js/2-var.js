bf59062572305c7fb3aa7dc6f91760cf60788dd0
Katarzyna <figarskak@gmail.com>
2018-02-10 16:55:34 +0100

    funkcje

--- zmienne/js/2-var.js
+++ zmienne/js/2-var.js
@@ -3,6 +3,31 @@ var nazwisko = "Nowacki";
 var wiek = 35;
 var wzrost;
 
+/*
+function pokazDane() {
+    console.log(imie, nazwisko, wiek);
+}
+
+pokazDane();
+
+imie = "Dariusz";
+nazwisko = "Kowalski";
+wiek = "27";
+
+pokazDane();
+
+*/
+
+/*
+function pokazDane() {
+    var imie = "Maciej";
+    console.log(imie);
+}
+
+console.log(imie);
+pokazDane();
+*/
+
 var wynik = "Imię: " + imie + " Nazwisko: " + nazwisko + " Wiek: " + wiek;
 console.log(imie);
 console.log(nazwisko);
