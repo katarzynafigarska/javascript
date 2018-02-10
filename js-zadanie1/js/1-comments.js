41d01e30dbec9df0c7792bfd22423a473431f07f
Katarzyna <figarskak@gmail.com>
2018-02-10 16:21:47 +0100

    zmienne

---
+++ js-zadanie1/js/1-comments.js
@@ -0,0 +1,11 @@
+/*
+Komentarz blokowy
+Dzięki niemu możemy nasz komentzarz umieścic  w wielu liniach 
+Skrót klawiatury CTRL + SHIFT /
+*/
+
+
+//komentarz liniowy, skończy sie razem z koncem tego wiersza, skrót CTRL + /
+tu już komentarz liniowy nie działa
+    
+    
