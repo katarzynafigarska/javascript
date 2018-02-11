789c8a7291b8bd28a35c8bcbbc470ab511af9174
Katarzyna <figarskak@gmail.com>
2018-02-11 09:14:36 +0100

    zmienne

---
+++ zmienne-2/js/3-var.js
@@ -0,0 +1,9 @@
+var wartosc = 15;
+
+console.log(9 + wartosc + 1);
+console.log(9 + wartosc + 2);
+console.log(9 * wartosc + wartosc - 2);
+console.log(90 + wartosc + 1);
+console.log(14 + wartosc + 2);
+console.log(wartosc + wartosc + 10);
+console.log(33 + wartosc + 21);
